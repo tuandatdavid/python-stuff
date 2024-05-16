f26755c9027eefbe9d427a300cc8338ea0a69de5
nokiapapir <88708244+tuandatdavid@users.noreply.github.com>
2024-05-16 09:43:27 +0200

    fwef

--- pi.py
+++ pi.py
@@ -1,1 +1,10 @@
 print("Hello, World")
+import math
+hem=input("R kružnice")
+hem=hem(int)
+pi=math.pi
+print(pi)
+def obsahkruhu(hem):
+    vysledek=(math.pi*hem*hem)
+    return vysledek
+print(obsahkruhu())
